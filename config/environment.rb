adea2484b4968828d4784bcb72f6c5a76e51a815
Henrik Loevborg <hlo@lenio.dk>
2009-05-04 16:17:12 +0200

    moved gems to env test

--- config/environment.rb
+++ config/environment.rb
@@ -19,10 +19,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "rspec", :lib => false, :version => ">=1.2.2"
-  config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
-  config.gem "webrat", :lib => false, :version => ">=0.4.3"
-  config.gem "cucumber", :lib => false, :version => ">=0.2.2"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
